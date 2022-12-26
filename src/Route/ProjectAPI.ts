1c6945179879c8b146f1a5a19c680204f4286c2f
yymin1022 <yymin1022@gmail.com>
2022-12-27 02:49:22 +0900

    Update ProjectAPI.ts : Improved Base API Functions

--- src/Route/ProjectAPI.ts
+++ src/Route/ProjectAPI.ts
@@ -7,12 +7,12 @@ projectRouter.get("/", (req: Request, res: Response) => {
     res.send("Project API");
 });
 
-memberRouter.post("/getData", (req: Request, res: Response) => {
-    res.send("Get Project Data");
-});
-
 memberRouter.post("/getList", (req: Request, res: Response) => {
     res.send("Get Project List");
+});
+
+memberRouter.post("/getProjectData", (req: Request, res: Response) => {
+    res.send("Get Specific Project Data");
 });
 
 export default projectRouter;
