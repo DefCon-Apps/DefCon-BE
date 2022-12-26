0fdf82af7f63ba4056fbc90175cbe2fc0c7f452f
yymin1022 <yymin1022@gmail.com>
2022-12-27 02:25:27 +0900

    Update ProjectAPI.ts : Added Base API Functions

--- src/Route/ProjectAPI.ts
+++ src/Route/ProjectAPI.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from "express";
+import memberRouter from "./MemberAPI";
 
 const projectRouter = express.Router();
 
 projectRouter.get("/", (req: Request, res: Response) => {
     res.send("Project API");
+});
+
+memberRouter.post("/getData", (req: Request, res: Response) => {
+    res.send("Get Project Data");
+});
+
+memberRouter.post("/getList", (req: Request, res: Response) => {
+    res.send("Get Project List");
 });
 
 export default projectRouter;
