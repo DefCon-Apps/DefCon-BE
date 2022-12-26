3d4073e9021aee0d4723610ba0d54b6f927bbce5
yymin1022 <yymin1022@gmail.com>
2022-12-27 02:24:55 +0900

    Update MemberAPI.ts : Added Base API Functions

--- src/Route/MemberAPI.ts
+++ src/Route/MemberAPI.ts
@@ -6,4 +6,12 @@ memberRouter.get("/", (req: Request, res: Response) => {
     res.send("Member API");
 });
 
+memberRouter.post("/getData", (req: Request, res: Response) => {
+    res.send("Get Member Data");
+});
+
+memberRouter.post("/getList", (req: Request, res: Response) => {
+    res.send("Get Member List");
+});
+
 export default memberRouter;
