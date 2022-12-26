929eef75ba880b90b0b2cf511183b43bc977dbcf
yymin1022 <yymin1022@gmail.com>
2022-12-27 02:23:14 +0900

    Update index.ts : Cleaned UP

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import express, {Request, Response} from "express";
 
 import memberRouter from "./Route/MemberAPI";
 import projectRouter from "./Route/ProjectAPI";
+import commonRouter from "./Route/CommonAPI";
 
 const app = express();
+
+app.use("/member", memberRouter);
+app.use("/project", projectRouter);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
 });
-
-app.use("/member", memberRouter);
-app.use("/project", projectRouter);
 
 app.listen(8080, () => {
     console.log("Server is Listening on Port 8080!");
