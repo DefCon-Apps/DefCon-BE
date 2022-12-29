230b6239a8b8320c88b3bb086d4d3074af0c077c
yymin1022 <yymin1022@gmail.com>
2022-12-29 17:42:27 +0900

    Update DataClass : Added Data Types

---
+++ src/Util/DataClass.ts
@@ -0,0 +1,31 @@
+interface MainEvent{
+
+}
+
+interface MemberData{
+
+}
+
+interface MemberHistoryData{
+
+}
+
+interface MemberList{
+
+}
+
+interface MemberListItem{
+
+}
+
+interface ProjectData{
+
+}
+
+interface ProjectList{
+
+}
+
+interface ProjectListItem{
+
+}
