f71aaa0d630e136e42d9b91a823624be5b05beb8
yymin1022 <yymin1022@gmail.com>
2022-12-30 04:03:39 +0900

    Update Project API : Added Transaction with Firebase Util on getList API Function

--- src/Route/ProjectAPI.ts
+++ src/Route/ProjectAPI.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
+import {API_DATA} from "../Util/DataClass";
+import * as FirebaseUtil from "../Util/FirebaseUtil";
 
 const projectRouter = express.Router();
 
-projectRouter.post("/getList", (req: Request, res: Response) => {
-    res.send("Get Project List");
+projectRouter.post("/getList", async (req: Request, res: Response) => {
+    const API_RESULT_DATA: API_DATA = await FirebaseUtil.getProjectList();
+
+    res.send(API_RESULT_DATA);
 });
 
 projectRouter.post("/getData", (req: Request, res: Response) => {
